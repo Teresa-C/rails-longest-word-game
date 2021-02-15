363d3123cab5658d222c5ad69df13dcdb2c493bf
Teresa Chang <teresachang1@yahoo.co.uk>
2021-02-15 20:34:29 +0000

    added controller, routes and views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    alphabet = ("A".."Z").to_a
+    @letters = alphabet.sample(10)
+  end
+
+  def score
+    @letters = params[:grid].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+end
